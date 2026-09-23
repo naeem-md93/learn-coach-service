dc3fc81b536d0b57fa46cd33bc65b66b519c6f30
naeem-md93 <naeem.mohammadi1993@gmail.com>
2026-09-24 02:37:54 +0330

    Move dotenv loading to manage.py and remove __main__.py

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+
+
+ENV_PATH = dotenv.find_dotenv(".env")
+ENV_LOADED = dotenv.load_dotenv(ENV_PATH)
+
+print(f"{ENV_LOADED=} | {ENV_PATH=}")
 
 
 def main():
